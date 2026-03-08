e86ac45ad979ea9c20a0c759e2154f5b06e474b6
Nuysa <nuysa.nutricion@gmail.com>
2026-03-07 21:06:13 -0500

    Force PWA full-screen mode and fix app icons synchronization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,15 +8,21 @@ export const metadata: Metadata = {
     manifest: "/manifest.json",
     appleWebApp: {
         capable: true,
-        statusBarStyle: "default",
+        statusBarStyle: "black-translucent",
         title: "NuySa",
     },
     formatDetection: {
         telephone: false,
     },
     icons: {
-        apple: "/icon-192x192.png",
-        icon: "/icon-192x192.png",
+        icon: [
+            { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
+            { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+        ],
+        shortcut: "/icon-192x192.png",
+        apple: [
+            { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
+        ],
     },
 };
 
